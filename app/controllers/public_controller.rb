3ba7887a9504a585bd8c56a94e181c630e06ce04
sirsen <sirsen@localhost.localdomain>
2017-02-12 15:30:16 +0300

    hound3

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,6 @@
 class PublicController < ApplicationController
   def index
     @card = Card.time_to_check.random.first
-    flash[:notice] = "Нет свежих карточек" if !@card
+    flash[:notice] = 'Нет свежих карточек' unless @card
   end
 end
